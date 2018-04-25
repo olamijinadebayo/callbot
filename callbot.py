64153766afaf76dd65b58a594ce466657382c9d2
olamijinadebayo <consultolamijin@gmail.com>
2018-04-25 09:47:08 +0300

    This will connect the SlackClient to the Realtime Api websocket connection then continuously loop and parses messages from the firehose to verify if any of the messages is directed at the bot

--- callbot.py
+++ callbot.py
@@ -18,3 +18,15 @@ TWIMLET = "https://twimlets.com/echo?Twiml=%3CResponse%3E%0A%20%20%3CDial%3E%3CC
 #instantiate Slack & Twilio clients
 slack_client= SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 twilio_client = TwilioRestClient()
+
+if __name__ =="__main__":
+    READ_WEBSOCKET_DELAY = 1 # this will give a second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("Callbot connected and running!")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID")
