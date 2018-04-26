c9660aca8de66702a40799ab6e14fb8c4a233f98
olamijinadebayo <consultolamijin@gmail.com>
2018-04-26 09:10:15 +0300

    This will modify the gitignore file

--- callbot.py
+++ callbot.py
@@ -19,12 +19,33 @@ TWIMLET = "https://twimlets.com/echo?Twiml=%3CResponse%3E%0A%20%20%3CDial%3E%3CC
 slack_client= SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 twilio_client = TwilioRestClient()
 
-def handle_command(command, channel):
+def handle_command(command,channel):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
+    response = "not sure what you mean *" +\
+                CALL_COMMAND + "* command with numbers, delimited by spaces."
+    if command.startswith(CALL_COMMAND):
+        response = 
+    slack_client.api_call("chat.postMessage", channel= channel,text=response,as_user=True)
+
+def parse_slack_output(slack_rtm_output):
     '''
     this  will get the commands directed at the bot and determines if they are valid commands.if
     they are directed at the bot it acts on the command else it returns back what it needs
     for the command to be verified
     '''
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip(),\
+                output['channel']
+
+    return None,None
 
 if __name__ =="__main__":
     READ_WEBSOCKET_DELAY = 1 # this will give a second delay between reading from firehose
